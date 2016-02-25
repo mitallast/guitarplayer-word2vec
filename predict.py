576bbe144130a1830d06f158a07072efa881d8dc
Alexey Korchevskiy <mitallast@gmail.com>
2016-02-25 16:23:12 +0300

    add predict.py

---
+++ predict.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import plyvel
+import re, string
+import sys, locale
+import word2vec
+import os
+import numpy as np
+
+reload(sys)
+sys.setdefaultencoding(locale.getdefaultlocale()[1])
+
+model_path = os.path.abspath('phrases.bin')
+model = word2vec.load(model_path)
+
+db = plyvel.DB('guitarplayer.db', create_if_missing=False)
+
+search = model['bare']
+
+for key, value in db:
+    value = value.decode('utf8')
+    words = re.findall(u'[a-zA-Zа-яА-Я0-9-]+', value.lower(), flags=re.UNICODE)
+    words = filter(lambda w: w in model, words)
+    if len(words) > 0:
+	vectors = map(lambda w: model.get_vector(w), words)
+	metrics = np.dot(vectors, search.T)
+	best = metrics[metrics.argmax()]
+	if best > 0.9:
+		print key, value
+	
+	
+db.close()
